b44d5e1dfebc86e50b5efa3374a288070e12a263
KovaliykSerhii <kovaliykserhii@gamil.com>
2023-11-28 17:11:12 -0500

    add new contact section

--- js/custom.js
+++ js/custom.js
@@ -2,35 +2,45 @@
 
   "use strict";
 
-    // COLOR MODE
-    $('.color-mode').click(function(){
-        $('.color-mode-icon').toggleClass('active')
-        $('body').toggleClass('dark-mode')
-    })
+  // COLOR MODE
+  $('.color-mode').click(function () {
+    $('.color-mode-icon').toggleClass('active')
+    $('body').toggleClass('dark-mode')
+  })
 
-    // HEADER
-    $(".navbar").headroom();
+  // HEADER
+  $(".navbar").headroom();
 
-    // PROJECT CAROUSEL
-    $('.owl-carousel').owlCarousel({
-    	items: 1,
-	    loop:true,
-	    margin:10,
-	    nav:true
-	});
+  // PROJECT CAROUSEL
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    nav: true
+  });
 
-    // SMOOTHSCROLL
-    $(function() {
-      $('.nav-link, .custom-btn-link').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 49
-        }, 1000);
-        event.preventDefault();
-      });
-    });  
+  // SMOOTHSCROLL
+  $(function () {
+    $('.nav-link, .custom-btn-link').on('click', function (event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 49
+      }, 1000);
+      event.preventDefault();
+    });
+  });
 
-    // TOOLTIP
-    $('.social-links a').tooltip();
+  // TOOLTIP
+  $('.social-links a').tooltip();
 
 })(jQuery);
+
+
+
+//ALERT
+var myLink = document.getElementById('getResume');
+
+myLink.addEventListener('click', function () {
+
+  alert('Soon!');
+});
